212e9ce446a40df0869c2e0384e167a4434163b4
matheusalves-digital <matheusalves@meirelesefreitas.adv.br>
2023-10-18 14:45:51 -0300

    feat: update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,66 +1,51 @@
-"use client"
-import Image from "next/image";
-import logo from "../../assets/logo.png";
-import { z } from "zod";
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useUsersData } from "@/hooks/useUsersData";
 import { PrimaryInputSearchIcon } from "@/components/primary-input";
 import { User, LockKeyhole } from "lucide-react";
-import { useUsersData } from "@/hooks/useUsersData";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 import { Logo } from "@/components/icons/logo-login";
 
-const validationScrema = z.object({
-  name: z
-    .string().min(5, "tem que ter 5 caracteres para continuar"),
-  senha: z
-    .string().min(8, "O Campo tem que conter no minimo 8 caracteres")
-        
-})
-.transform((field) => ({
-  name: field.name,
-  senha: field.senha
-}))
-
-
-type FormProps = z.infer<typeof validationScrema>
-type UserFormData = {
-  name: string,
-  senha: number
-}
-
-
-
 export function Login() {
-  const [name, setName] = useState<string>()
-  const [senha, setSenha] = useState<number>()
-  const { mutate, isSuccess } = useUsersData()
-  const router = useRouter()
-  
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const { mutate, isSuccess } = useUsersData();
+  const router = useRouter();
+
   const handleNavigate = () => {
-    router.push('/users')
-  }
+    router.push("/users");
+  };
 
-  const submit = () => {
-    const data = {
-      name,
-      senha,
+  const submit = async () => {
+    try {
+      const data = {
+        name,
+        password,
+      };
+
+      const response = await fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+
+        // Armazena o token (exemplo usando localStorage)
+        localStorage.setItem("token", responseData.token);
+
+        // Redireciona para a página de usuários
+        router.push("/users");
+      } else {
+        console.error("Falha na autenticação");
+      }
+    } catch (error) {
+      console.error("Erro durante a autenticação", error);
     }
-    mutate(data)
-    router.push('/users')
-  }
-
-
-  const { 
-    register, 
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormProps>({
-    criteriaMode: 'all',
-    mode: 'all',
-    resolver: zodResolver(validationScrema),
-  })
+  };
 
   return (
     <div className="flex w-80 h-96 bg-gray-900 p-10 flex-col rounded-lg">
@@ -68,33 +53,32 @@ export function Login() {
         <Logo />
       </div>
       <div className="flex mb-2 bg-gray-150 items-center rounded-lg border mt-4">
-        <User className="pl-2" size={25}/>
+        <User className="pl-2" size={25} />
         <PrimaryInputSearchIcon
           type="text"
           value={name}
           updateValue={setName}
           placeholder="Usuário"
-          {...register('name')}
         />
       </div>
-      {errors.name && <span className="mb-2 text-sm text-blue-50">{errors?.name?.message}</span>}
 
       <div className="flex bg-gray-150 items-center rounded-lg border">
-        <LockKeyhole className="pl-2" size={25}/>
+        <LockKeyhole className="pl-2" size={25} />
         <PrimaryInputSearchIcon
           type="password"
-          value={senha}
-          updateValue={setSenha}
+          value={password}
+          updateValue={setPassword}
           placeholder="Senha"
-          {...register('senha') }
         />
       </div>
-      <span className="text-sm text-blue-50 mb-2">{errors?.senha?.message}</span>
 
-      <button type="submit" className="h-10 rounded-lg bg-blue-50 text-gray-50 text-lg font-sans font-semibold" onClick={submit}>
+      <button
+        type="button"
+        className="h-10 rounded-lg bg-blue-50 text-gray-50 text-lg font-sans font-semibold"
+        onClick={submit}
+      >
         Entrar
       </button>
-    </div> 
-  )
-
+    </div>
+  );
 }
